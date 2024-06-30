c566876f0bad1821e387eba96398fb57391ad6bd
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-06-30 21:21:55 +0300

    Update ProfileContent.js

--- components/ProfileContent.js
+++ components/ProfileContent.js
@@ -51,8 +51,8 @@ export default function ProfileContent({activeTab,userId}) {
         <div>
           <Card>
             <h2 className="text-3xl mb-2">About me</h2>
-            <p className="mb-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut doloremque harum maxime mollitia perferendis praesentium quaerat. Adipisci, delectus eum fugiat incidunt iusto molestiae nesciunt odio porro quae quaerat, reprehenderit, sed.</p>
-            <p className="mb-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda error necessitatibus nesciunt quas quidem quisquam reiciendis, similique. Amet consequuntur facilis iste iure minima nisi non praesentium ratione voluptas voluptatem?</p>
+            <p className="mb-2 text-sm"></p>
+            <p className="mb-2 text-sm"></p>
           </Card>
         </div>
       )}
